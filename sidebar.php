ba9562c369507d7058106a189c69be252bbf89db
sonyadriko <sonyadiadriko@gmail.com>
2023-12-25 10:19:47 +0700

    Added a condition to display the 'Prediksi' link in the sidebar only for users with the '1' role. (generated by blackbox.ai )

--- sidebar.php
+++ sidebar.php
@@ -6,7 +6,9 @@
         <li class="header">Menu</li>
         <li> <a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> <span class="pull-right-container"></span> </a></li>       
         <li> <a href="penjualan.php"> <i class="fa fa-shopping-cart"></i> <span>Data Penjualan</span></a> </li>
+        <?php if($_SESSION['role'] == '1') { ?>
         <li> <a href="prediksi.php"><i class="fa fa-calculator"></i> <span>Prediksi</span> <span class="pull-right-container"></span> </a></li>
+      <?php } ?>
         <?php if($_SESSION['role'] == '2') { ?>
           <li> <a href="history.php"><i class="fa fa-history"></i> <span>History Ramal</span> <span class="pull-right-container"></span> </a></li>
       <?php } ?>
